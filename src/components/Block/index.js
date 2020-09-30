72742e5b20176f8303568a5af1ed888d3305300d
Muhammad Waqar <waqarilyas.dev@gmail.com>
2020-09-30 21:41:32 +0500

    solve virutalized list warning - NOT RECOMMENDED!

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 const Block = ({ children }) => {
   return (
     <View style={styles.container}>
-      <SafeAreaView>{children}</SafeAreaView>
+      <SafeAreaView />
+      <ScrollView
+        showsHorizontalScrollIndicato={false}
+        showsVerticalScrollIndicator={false}
+        nestedScrollEnabled>
+        {children}
+      </ScrollView>
     </View>
   );
 };
