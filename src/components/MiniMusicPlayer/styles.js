aea020dc0e08ff7996f4cad9b836011a51092e51
Muhammad Waqar <waqarilyas.dev@gmail.com>
2021-02-03 11:57:50 +0500

    some bugs fixed

--- src/components/MiniMusicPlayer/styles.js
+++ src/components/MiniMusicPlayer/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     position: 'absolute',
-    bottom: RFValue(85),
+    bottom: RFValue(Platform.OS === 'android' ? 50 : 65),
     alignItems: 'center',
     paddingHorizontal: RFValue(6),
     backgroundColor: 'black',
